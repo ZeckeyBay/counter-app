243fa049099f60878218e4046eec9d708e88272f
ZeckeyBay <zfatihturan@gmail.com>
2022-10-16 14:27:16 +0300

    reset added

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -9,12 +9,14 @@ function Counter() {
     <div>
     <h1>{count}</h1>
     <button onClick={()=> setCount(count+amount)}  >Increment</button>
+    <button onClick={() => setCount(0)}>Reset</button>
     <hr></hr>
     Amount: {amount}
     <br></br>
     <button onClick={()=> setAmount(1)}  >+1</button>
     <button onClick={()=> setAmount(3)}  >+3</button>
     <button onClick={()=> setAmount(5)}  >+5</button>
+    <button onClick={()=> setAmount(10)}  >+10</button>
     </div>
    
   )

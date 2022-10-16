e6978e2c284fb4d2eb0e599f02997b1146658221
ZeckeyBay <zfatihturan@gmail.com>
2022-10-16 13:42:21 +0300

    Components

---
+++ src/Components/Counter.js
@@ -0,0 +1,15 @@
+
+import React, {useState} from "react"
+
+function Counter() {
+  return (
+    <div>
+    <h1>{count}</h1>
+     <button>Increment</button>
+    
+    </div>
+   
+  )
+}
+
+export default Counter
